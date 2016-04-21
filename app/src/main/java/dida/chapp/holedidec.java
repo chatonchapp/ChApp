103d89e9dbeb4ca92a706c0bc9ce584f3baabbc9
davidhur10 <davidhur10@hotmail.es>
2016-04-21 18:32:26 +0200

    vprueba

---
+++ app/src/main/java/dida/chapp/holedidec.java
@@ -0,0 +1,13 @@
+package dida.chapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class holedidec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_holedidec);
+    }
+}
